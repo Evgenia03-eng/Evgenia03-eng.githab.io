79b1b65c67a7614e60c19bcad6459b2810862928
Evgenia03-eng <70372944+Evgenia03-eng@users.noreply.github.com>
2021-01-29 23:42:13 +0300

    Delete Unipro directory

--- Unipro/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(function(){
-        $('.info__item').each(function(){
-            $(this).on('click', function () {
-                $(this).toggleClass('info_show');
-            });
-        });
-    });
- });
